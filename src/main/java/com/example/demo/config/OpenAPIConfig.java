cb7817e93a7cab9787569ccfcc767992486988c5
bala-amypo <bala@amypo.in>
2025-12-21 12:43:39 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenAPIConfig.java
+++ src/main/java/com/example/demo/config/OpenAPIConfig.java
@@ -2,12 +2,18 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info =
-@Info(title = "Vendor Compliance API",
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Vendor Compliance API",
         version = "1.0",
-        description = "Vendor Document Compliance Validator"))
-public class OpenAPIConfig {
-}
+        description = "Vendor Document Compliance Validator"
+    ),
+    servers = {
+        @Server(url = "https://9462.pro604cr.amypo.ai/", description = "Production server")
+    }
+)
+public class OpenAPIConfig { }
